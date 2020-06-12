d162d8825fd6f9ff0f2e8c0a01e4efbd47b99a70
Arsalan Bin Najeeb <abnajeeb@knox.edu>
2020-06-12 14:54:50 -0500

    Updates

---
+++ precache-manifest.d93360bd22e695131d84ad4b19148152.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd65dca4d4fb131ccc1e33020c7327e5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c245c236b3545a9243c0",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "65ec02894f88f7aa57e1",
+    "url": "/static/js/2.47e36ee8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.47e36ee8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c245c236b3545a9243c0",
+    "url": "/static/js/main.53f1d370.chunk.js"
+  },
+  {
+    "revision": "9e5e3dc1feb330005169",
+    "url": "/static/js/runtime-main.04356979.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
